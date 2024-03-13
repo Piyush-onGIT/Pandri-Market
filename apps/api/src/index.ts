9e68b9131fd8cabb6516c0604f253739383ba161
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-03-13 12:14:00 +0530

    setup folders

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+
+const app: Express = express();
+app.use(express.json());
+
+const port = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ packages/eslint-config/api.js
@@ -0,0 +1,69 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  plugins: ["only-warn"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/"
+  ],
+  overrides: [
+    {
+      "env": {
+        "node": true
+      },
+      files: [
+        "*.js?(x)",
+        "*.ts?(x)",
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  rules: {
+    "indent": [
+      "error",
+      2
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
