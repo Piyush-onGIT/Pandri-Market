39a30c1f8edccb58e49b71dd4db99a4723a6bdfb
bmpskvas <khushi41bansal@gmail.com>
2024-03-15 20:01:07 +0530

    Dto added for user login & register

--- apps/api/src/http/errorHandler.ts
+++ apps/api/src/http/errorHandler.ts
@@ -3,7 +3,7 @@ import NotOK from "../response/notOk";
 import ApiError from "./ApiError";
 
 const errorHandler = (res: Response, error: ApiError) => {
-  return NotOK(res, error.message, error, error.statusCode);
+  return NotOK(res, error.message, error, error.statusCode ?? 500);
 };
 
 export default errorHandler;
