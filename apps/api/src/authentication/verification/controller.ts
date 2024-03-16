838e10778a3f88d8b29d3d97ffa040b2a21be92a
bmpskvas <khushi41bansal@gmail.com>
2024-03-17 00:21:32 +0530

    Otp and PhoneNo verification added

--- apps/api/src/authentication/verification/controller.ts
+++ apps/api/src/authentication/verification/controller.ts
@@ -6,45 +6,44 @@ import { Redis } from "ioredis";
 
 const redis = new Redis();
 function generateRandomNumber(): number {
-  let randomNumberStr: string = '';
+  let randomNumberStr: string = "";
   for (let i = 0; i < 4; i++) {
-      randomNumberStr += Math.floor(Math.random() * 10).toString();
+    randomNumberStr += Math.floor(Math.random() * 10).toString();
   }
   const randomNumber: number = parseInt(randomNumberStr);
   return randomNumber;
 }
-const  verifyPhoneNo =async  (req: any, res: Response) => {
-    const result = await axios.get(`http://localhost:5001/api/checkNumberStatus?phone=${req.body.phoneNo}&session=default`);
-    const phoneNoExist : Boolean= result.data.numberExists;
-    if(!phoneNoExist){
-        const error = new ApiError(401, "Invalid PhoneNO");
-      return errorHandler(res, error); 
-    }
-    else{
-      const otp=generateRandomNumber();
-      const requestingBody={
-          "chatId": `${req.body.phoneNo}@c.us`,
-          "text": `${otp}`,
-          "session": "default"
-      }
-      const result=await axios.post("http://localhost:5001/api/sendText",requestingBody);
-      redis.set(req.body.phoneNo,otp, 'EX', 60);
-      res.json({"message": "OTP sent"});
-      // console.log(result);
-    }
-  
+const verifyPhoneNo = async (req: any, res: Response) => {
+  const result = await axios.get(
+    `http://localhost:5001/api/checkNumberStatus?phone=${req.body.phoneNo}&session=default`
+  );
+  const phoneNoExist: boolean = result.data.numberExists;
+  if (!phoneNoExist) {
+    const error = new ApiError(401, "Invalid PhoneNO");
+    return errorHandler(res, error);
+  } else {
+    const otp = generateRandomNumber();
+    const requestingBody = {
+      chatId: `${req.body.phoneNo}@c.us`,
+      text: `${otp}`,
+      session: "default",
+    };
+    await axios.post("http://localhost:5001/api/sendText", requestingBody);
+    redis.set(req.body.phoneNo, otp, "EX", 60);
+    res.json({ message: "OTP sent" });
+    // console.log(result);
+  }
 };
-const verifyOtp = async (req:any,res:Response)=>{
-   const otp=req.body.otp;
-   const phoneNo=req.body.phoneNo;
-   const storedOtp =await redis.get(phoneNo);
-    console.log(phoneNo);
-     console.log(storedOtp);
-     if(storedOtp===otp){
-      res.json({"message": "PhoneNo Verified"});
-    }
-    else{
-      res.json({"message": "OTP invalid"});
-    }
-}
-export {verifyPhoneNo,verifyOtp};
+const verifyOtp = async (req: any, res: Response) => {
+  const otp = req.body.otp;
+  const phoneNo = req.body.phoneNo;
+  const storedOtp = await redis.get(phoneNo);
+  console.log(phoneNo);
+  console.log(storedOtp);
+  if (storedOtp === otp) {
+    res.json({ message: "PhoneNo Verified" });
+  } else {
+    res.json({ message: "OTP invalid" });
+  }
+};
+export { verifyPhoneNo, verifyOtp };
