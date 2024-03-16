fde532c306ba4b10cbf7d646121a772e58f48a99
Prince <princetripathi093@gmail.com>
2024-03-16 12:28:19 +0530

    image uploader added using multer

--- apps/api/src/uploads/controller.ts
+++ apps/api/src/uploads/controller.ts
@@ -1,40 +1,5 @@
 import { Request, Response } from "express";
 import multer from "multer";
-// import {
-//   GetObjectCommand,
-//   PutObjectCommand,
-//   S3Client,
-// } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
-// const s3Client = new S3Client({
-//   credentials: {
-//     accessKeyId: `${process.env.AWS_ACCESS_KEY}`,
-//     secretAccessKey: `${process.env.AWS_SECRET_KEY}`,
-//   },
-// });
-
-// async function getObjectURL(key: any) {
-//   const command = new GetObjectCommand({
-// Bucket: `${process.env.AWS_BUCKET_NAME}`,
-//     Key: key,
-//   });
-
-//   const url = await getSignedUrl(s3Client, command);
-//   return url;
-// }
-
-// const putObjet = async (filename: any, contentType: any) => {
-//   const command = new PutObjectCommand({
-//     Bucket: "pandri-market-bucket",
-//     Key: `./uploads/user/${filename}`,
-//     ContentType: contentType,
-//   });
-//   const url = await getSignedUrl(s3Client, command);
-//   return url;
-// };
-
-// const uploadToS3 = async () => {};
 
 const storage = multer.diskStorage({
   destination: function (req: any, file: any, cb: any) {
