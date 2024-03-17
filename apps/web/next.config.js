a40ac879eaae0520425a15b7532141863c5eb96d
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-03-17 22:23:44 +0530

    added shared tailwind

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: [
+    "@repo/ui",
+    "@repo/tailwind-config",
+    "@repo/nextui-config",
+  ],
 };
