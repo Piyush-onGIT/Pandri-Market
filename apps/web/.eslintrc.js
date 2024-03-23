b4bcba9dbae7ec11c06d74774dc4ef77874f5e84
Prince <princetripathi093@gmail.com>
2024-03-23 20:55:11 +0530

    stash

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -6,5 +6,5 @@ module.exports = {
   parserOptions: {
     project: true,
   },
-  ignorePatterns: ["tsconfig.json", "**/useStore.ts"],
+  ignorePatterns: ["tsconfig.json", "**/useStore.ts", "useStore"],
 };
