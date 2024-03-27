052240292c7ed67b7c19a7a46f613af9b5a193e8
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-03-27 22:42:14 +0530

    stash

--- apps/api/src/shops/controllers.ts
+++ apps/api/src/shops/controllers.ts
@@ -137,7 +137,6 @@ const posts = async (req: Request, res: Response) => {
       if (!postCreated) {
         throw new ApiError(500, "Unable to post please try again");
       }
-
       user.credit = user.credit - reduction;
       await user.save();
       return res.status(200).json({
