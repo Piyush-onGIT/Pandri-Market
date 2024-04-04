beddc2c56b4cc7bb0e28596022442336f4dd5b88
Piyush-onGIT <piyush.at.googl@gmail.com>
2024-04-04 17:04:40 +0530

    adding logger

--- apps/api/src/http/errorHandler.ts
+++ apps/api/src/http/errorHandler.ts
@@ -1,9 +1,16 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import NotOK from "../response/notOk";
 import ApiError from "./ApiError";
+import Logger from "../utils/logger";
 
-const errorHandler = (res: Response, error: ApiError) => {
-  return NotOK(res, error.message, error, error.statusCode ?? 500);
+const errorHandler = (res: Response, error: ApiError, req?: Request) => {
+  const copyError = { ...error };
+  if (req) {
+    console.log(req.url);
+    const error1: any = { x: { 1: 2 }, y: 1, z: [1, "2"] };
+    Logger.error(`${req.url} -> ${error1}`);
+  } else Logger.error(error);
+  return NotOK(res, copyError.message, copyError, copyError.statusCode ?? 500);
 };
 
 export default errorHandler;
